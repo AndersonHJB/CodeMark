18a8decc39197fc364bd3fb04836de0738bcf1cb
AndersonHJB <aiyuechuang@gmail.com>
2024-07-20 11:32:31 +0800

    同步

--- CodeMark/urls.py
+++ CodeMark/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from mdeditor import views
+
+router = routers.DefaultRouter()
+router.register(r'markdown', views.MarkdownDocumentViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', include(router.urls)),
 ]
