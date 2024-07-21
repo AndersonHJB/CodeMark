dce75e3f1243601a23805a1cca349d4715a0ee6e
AndersonHJB <cleland1432803776@icloud.com>
2024-07-21 16:52:48 +0800

    更新部署脚本

--- cp_opt.sh
+++ cp_opt.sh
@@ -2,3 +2,7 @@ cd /home/huangjiabao/domains/cm.class1v1.com/public_python/CodeMark
 git pull
 cd ..
 cp -r ./CodeMark/* .
+cd /home/huangjiabao/domains/cm.bornforthis.cn/public_python/CodeMark
+git pull
+cd ..
+cp -r ./CodeMark/* .
