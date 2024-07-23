a1496f316070f1224921ca29959bf674af86b7c1
AndersonHJB <cleland1432803776@icloud.com>
2024-07-23 19:46:05 +0800

    扩张 Markdown

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def index():
 def article(filename):
     with open(os.path.join('articles', filename), 'r') as f:
         content = f.read()
-        html_content = markdown.markdown(content, extensions=['fenced_code', 'codehilite'])
+        html_content = markdown.markdown(content, extensions=['extra', 'codehilite', 'toc', 'tables', 'fenced_code'])
     return render_template('article.html', content=html_content)
 
 
